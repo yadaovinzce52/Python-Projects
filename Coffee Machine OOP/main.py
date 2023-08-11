2bf4a673a939c022a73158f8b02ed0b8edc3de00
Vinzce Yadao <yadaovinzce@gmail.com>
2023-08-11 13:11:00 -0400

    Completed Coffee Machine Project

---
+++ Coffee Machine OOP/main.py
@@ -0,0 +1,25 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+register = MoneyMachine()
+
+
+def reports():
+    coffee_maker.report()
+    register.report()
+
+
+while True:
+    choice = input(f'What would you like? ({menu.get_items()}): ').lower()
+    if choice == 'off':
+        break
+    elif choice == 'report':
+        reports()
+    elif choice in menu.get_items().split('/'):
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if register.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
--- Vending Machine/mainOOP.py
+++
@@ -1,8 +0,0 @@
-class MenuItem:
-    def __init__(self, name, cost, ingredients):
-        self.name = name
-        self.cost = cost
-        self.ingredients = ingredients
-
-class Menu:
-    def get_items(self):
