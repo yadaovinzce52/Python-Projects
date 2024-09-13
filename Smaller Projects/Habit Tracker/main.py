333ebf3305c346f85ab4d6d707bb8094bfca8d7c
Vinzce Yadao <yadaovinzce@gmail.com>
2024-09-13 12:27:36 -0400

    Completed Simple Habit Tracker project

---
+++ Smaller Projects/Habit Tracker/main.py
@@ -0,0 +1,60 @@
+import requests
+import datetime as dt
+
+TOKEN = 'abcdefghijklmnopqrstuvwxyz'
+USERNAME = 'yadaov52'
+
+pixela_endpoint = 'https://pixe.la/v1/users'
+
+user_params = {
+    'token': TOKEN,
+    'username': USERNAME,
+    'agreeTermsOfService': 'yes',
+    'notMinor': 'yes'
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs'
+
+graph_config = {
+    'id': 'graph1',
+    'name': 'Take courses',
+    'unit': 'hours',
+    'type': 'int',
+    'color': 'sora'
+}
+
+headers = {
+    'X-USER-TOKEN': TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, headers=headers, json=graph_config)
+# print(response.text)
+
+pixel_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs/{graph_config["id"]}'
+
+today = dt.datetime.now()
+
+pixel_body = {
+    'date': today.strftime("%Y%m%d"),
+    'quantity': input('How many hours did you do programming related tasks? ')
+}
+
+response = requests.post(pixel_endpoint, json=pixel_body, headers=headers)
+print(response.text)
+
+update_pixel_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs/graph1/20240912'
+
+pixel_update = {
+    'quantity': '8',
+}
+
+# response = requests.put(update_pixel_endpoint, json=pixel_update, headers=headers)
+# print(response.text)
+
+delete_pixel = f'{pixela_endpoint}/{USERNAME}/graphs/graph1/20240912'
+
+# response = requests.delete(delete_pixel, headers=headers)
+# print(response.text)
